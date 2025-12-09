7bb56fe211ff95905a48cc06888c8e465c653a82
rami <rami.benhadjabdallah@vocanto.de>
2025-12-09 12:36:04 +0100

    Initial project setup

---
+++ app/index.tsx
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello World</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 24,
+    color: '#000000',
+  },
+});
+
